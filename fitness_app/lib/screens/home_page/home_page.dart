09dc48f602aa07a194c6a9d31cee1ad1dcc97a0e
Praveen Kumar <praveen.kumar30@sap.com>
2021-12-15 23:33:31 +0530

    firebase

---
+++ fitness_app/lib/screens/home_page/home_page.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fitness_app/widgets/google_sign_in.dart';
+import 'package:fitness_app/widgets/logged_in_widget.dart';
+import 'package:fitness_app/widgets/sign_up_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatelessWidget {
+  static const String id = 'HomePageScreen';
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    body: ChangeNotifierProvider(
+      create: (context) => GoogleSignInProvider(),
+      child: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          final provider = Provider.of<GoogleSignInProvider>(context);
+
+          if (provider.isSigningIn) {
+            return buildLoading();
+          } else if (snapshot.hasData) {
+            return LoggedInWidget();
+          } else {
+            return SignUpWidget();
+          }
+        },
+      ),
+    ),
+  );
+
+  Widget buildLoading() => Stack(
+    fit: StackFit.expand,
+    children: [
+      Center(child: CircularProgressIndicator()),
+    ],
+  );
+}
---
+++ fitness_app/lib/widgets/logged_in_widget.dart
@@ -0,0 +1,63 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fitness_app/widgets/side_drawer.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class LoggedInWidget extends StatelessWidget {
+  static const String id = 'LoggedInScreen';
+  @override
+  Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
+    if(user==null){
+      var defaultImage = "";
+      var defaultName = "Anonymous";
+    }
+
+    return SafeArea(
+      child: Scaffold(
+        drawer: SideDrawer(),
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('the fitNESS app'),
+          actions: [ RaisedButton(
+            color: Colors.deepOrange,
+            child: CircleAvatar(
+              maxRadius: 25,
+              backgroundImage: NetworkImage(""),
+            ),
+            onPressed:(){
+              //go to edit profile
+            },
+          ),
+
+
+          ],
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          color: Colors.blueGrey.shade900,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Text(
+                  'Welcome '  + '!',
+                  style: TextStyle(color: Colors.white,
+                      fontWeight: FontWeight.w300,
+                      fontSize: 20),
+                ),
+              ),
+
+
+              SizedBox(height: 80),
+
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
