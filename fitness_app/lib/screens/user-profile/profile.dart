21470ad0a2b4b68426ca1a281ff9eddf217c8a45
Nousheen Sultana <nousheen.sultana@sap.com>
2021-12-15 17:30:44 +0530

    Profile page

---
+++ fitness_app/lib/screens/user-profile/profile.dart
@@ -0,0 +1,211 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+//import 'package:flutter/chart.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+class ProfileApp extends StatelessWidget {
+
+  const ProfileApp({Key? key}) : super(key: key);
+
+  Duration get loginTime => const Duration(milliseconds: 2250);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [Colors.deepOrange, Colors.pinkAccent]
+                  )
+              ),
+              child: SizedBox(
+                width: double.infinity,
+                height: 350.0,
+                child: Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // CircleAvatar(
+                      //   backgroundImage: NetworkImage(
+                      //     "https://www.trendrr.net/wp-content/uploads/2017/06/Deepika-Padukone-1.jpg",
+                      //   ),
+                      //   radius: 50.0,
+                      // ),
+                      const SizedBox(
+                        height: 10.0,
+                      ),
+                      const Text(
+                        "Natasha",
+                        style: TextStyle(
+                          fontSize: 22.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10.0,
+                      ),
+                      Card(
+                        margin: const EdgeInsets.symmetric(horizontal: 20.0,vertical: 5.0),
+                        clipBehavior: Clip.antiAlias,
+                        color: Colors.white,
+                        elevation: 5.0,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0,vertical: 22.0),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Column(
+
+                                  children: const [
+                                    Text(
+                                      "Height",
+                                      style: TextStyle(
+                                        color: Colors.redAccent,
+                                        fontSize: 22.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 5.0,
+                                    ),
+                                    Text(
+                                      "5'2",
+                                      style: TextStyle(
+                                        fontSize: 20.0,
+                                        color: Colors.pinkAccent,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Expanded(
+                                child: Column(
+
+                                  children: const [
+                                    Text(
+                                      "Weight",
+                                      style: TextStyle(
+                                        color: Colors.redAccent,
+                                        fontSize: 22.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 5.0,
+                                    ),
+                                    Text(
+                                      "52KG",
+                                      style: TextStyle(
+                                        fontSize: 20.0,
+                                        color: Colors.pinkAccent,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Expanded(
+                                child: Column(
+
+                                  children: const [
+                                    Text(
+                                      "Age",
+                                      style: TextStyle(
+                                        color: Colors.redAccent,
+                                        fontSize: 22.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 5.0,
+                                    ),
+                                    Text(
+                                      "24",
+                                      style: TextStyle(
+                                        fontSize: 20.0,
+                                        color: Colors.pinkAccent,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 30.0,horizontal: 16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  "Moto",
+                  style: TextStyle(
+                      color: Colors.redAccent,
+                      fontStyle: FontStyle.normal,
+                      fontSize: 28.0
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text('Exercise should be regarded as tribute to the heart',
+                  style: TextStyle(
+                    fontSize: 22.0,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.w300,
+                    color: Colors.black,
+                    letterSpacing: 2.0,
+                  ),
+                ),
+
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          SizedBox(
+            width: 300.00,
+
+            child: RaisedButton(
+                onPressed: (){},
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(80.0)
+                ),
+                elevation: 0.0,
+                padding: const EdgeInsets.all(0.0),
+                child: Ink(
+                  decoration: BoxDecoration(
+                    gradient: const LinearGradient(
+                        begin: Alignment.centerRight,
+                        end: Alignment.centerLeft,
+                        colors: [Colors.pink,Colors.pinkAccent]
+                    ),
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  child: Container(
+                    constraints: BoxConstraints(maxWidth: 300.0, minHeight: 50.0),
+                    alignment: Alignment.center,
+                    child: const Text("Edit Profile",
+                      style: TextStyle(color: Colors.white, fontSize: 26.0, fontWeight:FontWeight.w300),
+                    ),
+                  ),
+                )
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
