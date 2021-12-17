d07f811906bff019374e604b8af0e309ec01c9de
nousheensulthana <38486648+nousheensulthana@users.noreply.github.com>
2021-12-17 21:04:51 +0530

    Updating profile.dart

--- fitness_app/lib/screens/user-profile/profile.dart
+++ fitness_app/lib/screens/user-profile/profile.dart
@@ -170,10 +170,32 @@ class ProfileApp extends StatelessWidget {
             SizedBox(
                 height: 300.0,
                 width: 280.0,
-                child: SfCartesianChart(),
+                child: SfCartesianChart(                    
+                    primaryXAxis: CategoryAxis(),
+                    series: <LineSeries<SalesData, String>>[
+                      LineSeries<SalesData, String>(
+                        // Bind data source
+                          dataSource: <SalesData>[
+                            SalesData('Jan', 35),
+                            SalesData('Feb', 28),
+                            SalesData('Mar', 34),
+                            SalesData('Apr', 32),
+                            SalesData('May', 40)
+                          ],
+                          xValueMapper: (SalesData sales, _) => sales.year,
+                          yValueMapper: (SalesData sales, _) => sales.sales
+                          
+                      )
+                    ]),
             ),
         ],
       ),
     );
   }
+}
+
+class SalesData {
+  SalesData(this.year, this.sales);
+  final String year;
+  final double sales;
 }
